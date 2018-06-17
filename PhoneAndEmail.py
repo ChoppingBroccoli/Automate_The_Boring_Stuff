9a1cb3dfe3407879a15435e6ab8cbc2f17beac57
Justin <ChoppingBroccoli@users.noreply.github.com>
2018-06-17 00:06:41 -0400

    Udemy Phone_Email Regex Practice Proj - Complete
    Completed
    http://automatetheboringstuff.com/chapter7/

--- PhoneAndEmail.py
+++ PhoneAndEmail.py
@@ -1,5 +1,3 @@
-#! /usr/bin/python3
-
 import re, pyperclip
 
 # Create a regex for phone numbers
@@ -8,31 +6,35 @@ phoneRegex = re.compile(r'''
 # Since there are groups, findall() will return a list of tuples for the phone numbers.
 # A fix for this is to put the entire regex into a group.
 (
-((\d\d\d) | (\(\d\d\d\)))? # area code (optional)(3 digits no parens OR 3 digits with parens)
-(\s | -) # first separator (space OR hyphen)
+((\d\d\d)|(\(\d\d\d\)))? # area code (optional)(3 digits no parens OR 3 digits with parens)
+(\s|-) # first separator (space OR hyphen)
 \d\d\d # first 3 digits
 - # separator
 \d\d\d\d #last 4 digits
-(((ext(\.)?\s) | x) # extenstion with verbiage (optional)('ext' with/without a period OR just an 'x')
+(((ext(\.)?\s)|x) # extenstion with verbiage (optional)('ext' with/without a period OR just an 'x')
 (\d{2,5}))? # extension only digits (optional)(between 2 - 5 digits)
 )
 ''', re.VERBOSE)
 
 # Create a regex for email addresses
 emailRegex = re.compile(r'''
-# name@domain.com (include .+_-)
-
-[a-zA-Z0-9_.+-]+	# name part
+# name@domain.com (include .+_)
+[a-zA-Z0-9_.+]+	# name part
 @ # @ symbol
-[a-zA-Z0-9_.+-]+ # domain part
+[a-zA-Z0-9_.+]+ # domain part
 ''', re.VERBOSE)
 
 # Get text off the clipboard
 text = pyperclip.paste()
 
 # Extract email/phone number from the text
-phoneRegex.findall(text) # returns a list of strings
-
-# Copy extracted email/phone to the clipboard
 extractedPhone = phoneRegex.findall(text)
 extractedEmail = emailRegex.findall(text)
+
+allPhoneNumbers = []
+for phoneNumber in extractedPhone:
+    allPhoneNumbers.append(phoneNumber[0])
+
+# Copy extracted email/phone to the clipboard
+results = '\n'.join(allPhoneNumbers) + '\n' + '\n'.join(extractedEmail)
+pyperclip.copy(results)
