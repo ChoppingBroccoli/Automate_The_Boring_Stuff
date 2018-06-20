41b688f0767e2e97dc18ac5a9ca2e74d1a1b93c7
Justin <ChoppingBroccoli@users.noreply.github.com>
2018-06-20 19:56:31 -0400

    Updated comments
    Explained some things better by updating the comments

--- CH7_Prac_01_URLs.py
+++ CH7_Prac_01_URLs.py
@@ -4,16 +4,16 @@ import re, pyperclip
 
 # create a regex
 httpRegex = re.compile(r'''
-((http://|https://)\w+ # determine http:// or https://
-\. # period before TLD
+((http://|https://)\w+ # match http:// or https:// ((http://|https://) or word characters (\w) but there has to be at least one match (+)
+\. # this is the period before TLD
 (com|net|org|gov))    # choose either .com, .net, or .org
 ''', re.VERBOSE)
 
-text = pyperclip.paste() # paste contents of clipboard
-found_URL = httpRegex.findall(text) # returns match objects
+text = pyperclip.paste() # paste contents of clipboard to text variable
+found_URL = httpRegex.findall(text) # returns match objects based on strings in text variable and stored match objects in found_URL variable
 
-# clean up the output by iterating through each tuple from found_URL to extract only the first item
-all_URLs = []
+# clean up the output by iterating through each tuple from found_URL and extracting only the first item
+all_URLs = [] # initialize all_URLs variable with an empty list
 for each_URL in found_URL:
     all_URLs.append(each_URL[0])
 
